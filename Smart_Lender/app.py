fd4a4cbcc856b8bcbc27acf20101df569f1bd550
PushpanjaliCoder <pushpanjalipushpa7@gmail.com>
2026-07-10 14:37:54 +0530

    Add files via upload

--- Smart_Lender/app.py
+++ Smart_Lender/app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request
 import joblib
-import os
 
 app = Flask(__name__)
 
@@ -88,5 +87,4 @@ def predict():
     return render_template("result.html", result=result)
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run()
